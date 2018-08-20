249307a5621bb19b6c94841b2968ad363ed280f4
Akash Soni <akashsoni@novomac9s-macbook-pro-2.local>
2018-08-20 12:31:06 +0530

    pod installed

--- VdocipherPOC/ViewController.swift
+++ VdocipherPOC/ViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import MBProgressHUD
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ VdocipherPOC/Vocabimate--BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  Vocabimate--BridgingHeader.h
+//  VdocipherPOC
+//
+//  Created by Akash Soni on 20/08/18.
+//  Copyright © 2018 Akash Soni. All rights reserved.
+//
+
+#ifndef Vocabimate__BridgingHeader_h
+#define Vocabimate__BridgingHeader_h
+
+#import "MBProgressHUD/MBProgressHUD.h"
+
+#endif /* Vocabimate__BridgingHeader_h */
